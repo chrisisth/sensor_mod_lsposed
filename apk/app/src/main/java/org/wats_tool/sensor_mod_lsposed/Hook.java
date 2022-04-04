67c9bbffdb153c28ee41a71dbd373e0ead4dbc24
sceext <sceext@foxmail.com>
2022-04-04 22:38:29 +0800

    尝试 hook 更多 API

--- apk/app/src/main/java/org/wats_tool/sensor_mod_lsposed/Hook.java
+++ apk/app/src/main/java/org/wats_tool/sensor_mod_lsposed/Hook.java
@@ -4,10 +4,14 @@ import java.util.HashSet;
 
 import android.os.Process;
 import android.os.Handler;
+import android.os.MemoryFile;
+import android.hardware.HardwareBuffer;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorDirectChannel;
+import android.hardware.SensorManager.DynamicSensorCallback;
+import android.hardware.TriggerEventListener;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import de.robv.android.xposed.XposedBridge;
@@ -17,7 +21,7 @@ import de.robv.android.xposed.XC_MethodHook;
 
 public class Hook implements IXposedHookLoadPackage {
     private final String LOG_TAG = "sensor_mod  ";
-    private final String P_VERSION = "sensor_mod version 0.1.0-a4";
+    private final String P_VERSION = "sensor_mod version 0.1.0-a5";
 
     private final ConfigHost ch = new ConfigHost();
 
@@ -29,12 +33,11 @@ public class Hook implements IXposedHookLoadPackage {
         int pid = Process.myPid();
         // 避免重复 hook
         if (pidSet.contains(pid)) {
-            XposedBridge.log(LOG_TAG + "#### pid " + pid + "  sikp hook  packageName: " + p.packageName);
+            XposedBridge.log(LOG_TAG + "#### pid " + pid + "  skip hook  packageName: " + p.packageName);
             return;
         }
         pidSet.add(pid);
 
-        // 不挑选 hook 目标
         XposedBridge.log(LOG_TAG + "#### pid " + pid + "  HOOK " + P_VERSION + "  packageName: " + p.packageName);
 
         // protected boolean registerListenerImpl(
@@ -90,8 +93,166 @@ public class Hook implements IXposedHookLoadPackage {
             }
         });
 
+        // 用于测试
+        debugHook(p);
+
         // 加载配置
         ch.load();
+    }
+
+    private void debugHook(final LoadPackageParam p) {
+        // #### android.hardware.SensorManager
+        // https://github.com/aosp-mirror/platform_frameworks_base/blob/android10-d4-s1-release/core/java/android/hardware/SensorManager.java
+
+        // public List<Sensor> getSensorList(int type)
+        findAndHookMethod("android.hardware.SensorManager", p.classLoader, "getSensorList",
+            int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam pa) throws Throwable {
+                XposedBridge.log(LOG_TAG + "SensorManager.getSensorList()");
+                // TODO
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam pa) throws Throwable {}
+        });
+
+        // public List<Sensor> getDynamicSensorList(int type)
+        findAndHookMethod("android.hardware.SensorManager", p.classLoader, "getDynamicSensorList",
+            int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam pa) throws Throwable {
+                XposedBridge.log(LOG_TAG + "SensorManager.getDynamicSensorList()");
+                // TODO
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam pa) throws Throwable {}
+        });
+
+        // public Sensor getDefaultSensor(int type)
+        findAndHookMethod("android.hardware.SensorManager", p.classLoader, "getDefaultSensor",
+            int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam pa) throws Throwable {
+                XposedBridge.log(LOG_TAG + "SensorManager.getDefaultSensor(" + pa.args[0] + ")");
+                // TODO
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam pa) throws Throwable {}
+        });
+
+        // public Sensor getDefaultSensor(int type, boolean wakeUp)
+        findAndHookMethod("android.hardware.SensorManager", p.classLoader, "getDefaultSensor",
+            int.class, boolean.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam pa) throws Throwable {
+                XposedBridge.log(LOG_TAG + "SensorManager.getDefaultSensor(" + pa.args[0] + ", " + pa.args[1] + ")");
+                // TODO
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam pa) throws Throwable {}
+        });
+
+        // public boolean registerListener(SensorEventListener listener, Sensor sensor, int samplingPeriodUs, int maxReportLatencyUs)
+        findAndHookMethod("android.hardware.SensorManager", p.classLoader, "registerListener",
+            SensorEventListener.class, Sensor.class, int.class, int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam pa) throws Throwable {
+                XposedBridge.log(LOG_TAG + "SensorManager.registerListener()  1");
+                // TODO
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam pa) throws Throwable {}
+        });
+
+        // public boolean registerListener(SensorEventListener listener, Sensor sensor, int samplingPeriodUs, Handler handler)
+        findAndHookMethod("android.hardware.SensorManager", p.classLoader, "registerListener",
+            SensorEventListener.class, Sensor.class, int.class, Handler.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam pa) throws Throwable {
+                XposedBridge.log(LOG_TAG + "SensorManager.registerListener()  2");
+                // TODO
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam pa) throws Throwable {}
+        });
+
+        // public boolean registerListener(SensorEventListener listener, Sensor sensor, int samplingPeriodUs, int maxReportLatencyUs, Handler handler)
+        findAndHookMethod("android.hardware.SensorManager", p.classLoader, "registerListener",
+            SensorEventListener.class, Sensor.class, int.class, int.class, Handler.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam pa) throws Throwable {
+                XposedBridge.log(LOG_TAG + "SensorManager.registerListener()  3");
+                // TODO
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam pa) throws Throwable {}
+        });
+
+        // public SensorDirectChannel createDirectChannel(MemoryFile mem)
+        findAndHookMethod("android.hardware.SensorManager", p.classLoader, "createDirectChannel",
+            MemoryFile.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam pa) throws Throwable {
+                XposedBridge.log(LOG_TAG + "SensorManager.createDirectChannel()  1");
+                // TODO
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam pa) throws Throwable {}
+        });
+
+        // public SensorDirectChannel createDirectChannel(HardwareBuffer mem)
+        findAndHookMethod("android.hardware.SensorManager", p.classLoader, "createDirectChannel",
+            HardwareBuffer.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam pa) throws Throwable {
+                XposedBridge.log(LOG_TAG + "SensorManager.createDirectChannel()  2");
+                // TODO
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam pa) throws Throwable {}
+        });
+
+        // public void registerDynamicSensorCallback(DynamicSensorCallback callback, Handler handler)
+        findAndHookMethod("android.hardware.SensorManager", p.classLoader, "registerDynamicSensorCallback",
+            DynamicSensorCallback.class, Handler.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam pa) throws Throwable {
+                XposedBridge.log(LOG_TAG + "SensorManager.registerDynamicSensorCallback()");
+                // TODO
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam pa) throws Throwable {}
+        });
+
+        // public boolean requestTriggerSensor(TriggerEventListener listener, Sensor sensor)
+        findAndHookMethod("android.hardware.SensorManager", p.classLoader, "requestTriggerSensor",
+            TriggerEventListener.class, Sensor.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam pa) throws Throwable {
+                XposedBridge.log(LOG_TAG + "SensorManager.requestTriggerSensor()");
+                // TODO
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam pa) throws Throwable {}
+        });
+
+        // #### android.hardware.SystemSensorManager
+        // https://github.com/aosp-mirror/platform_frameworks_base/blob/android10-d4-s1-release/core/java/android/hardware/SystemSensorManager.java
+
+        // TODO
+        // findAndHookMethod("android.hardware.SystemSensorManager", p.classLoader, "",
+        //     SensorDirectChannel.class, Sensor.class, int.class,
+        // new XC_MethodHook() {
+        //     @Override
+        //     protected void beforeHookedMethod(MethodHookParam pa) throws Throwable {
+        //         XposedBridge.log(LOG_TAG + "SystemSensorManager.");
+        //         // TODO
+        //     }
+        //     @Override
+        //     protected void afterHookedMethod(MethodHookParam pa) throws Throwable {}
+        // });
+
+        // TODO
     }
 
     public String getFullClassName(SensorEventListener listener) {
